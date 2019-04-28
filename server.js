619f7bf2362af48465798cb8b39a83a6d3131cdd
itsaiub <itsaiub@gmail.com>
2019-04-28 08:28:20 +0600

    mongodb connect

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const app = express();
+app.use(morgan("dev"));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 const PORT = process.env.PORT || 4000;
 
 app.get("/", (req, res) => {
@@ -10,4 +20,11 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`server is running on PORT ${PORT}`);
+  mongoose.connect(
+    "mongodb://localhost/money-management-app",
+    { useNewUrlParser: true },
+    () => {
+      console.log("database connected");
+    }
+  );
 });
