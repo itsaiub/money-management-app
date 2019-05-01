b454a09671cf6226380bba718a1ccd3b98a9ad47
itsaiub <itsaiub@gmail.com>
2019-05-01 12:12:40 +0600

    client routes setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import LogIn from "./pages/LogIn";
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Hello From React</h1>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/login" component={LogIn} />
+          <Route path="/register" component={Register} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
--- client/src/pages/LogIn.js
+++ client/src/pages/LogIn.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Home = () => {
+const LogIn = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <h1>LogIn</h1>
     </div>
   );
 };
 
-export default Home;
+export default LogIn;
